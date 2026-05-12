411b3dbf87ea9b3d84818c0e0606d64d904b9d13
KyleYu2024 <152136955+KyleYu2024@users.noreply.github.com>
2026-05-12 21:32:24 +0800

    Update __init__.py

--- plugins.v2/plugin115sub/__init__.py
+++ plugins.v2/plugin115sub/__init__.py
@@ -17,7 +17,7 @@ class Plugin115Sub(_PluginBase):
     plugin_desc = "将 MoviePilot 与 115sub 进行订阅、下载、占位和完成态双向联动。"
     plugin_icon = "link.png"
     plugin_version = "0.1.3"
-    plugin_author = "KyleYu2024"
+    plugin_author = "KyleYu"
     author_url = "https://github.com/KyleYu2024/MoviePilot-Plugins"
     plugin_config_prefix = "plugin115sub_"
     plugin_order = 10
